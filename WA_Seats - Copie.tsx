ec8ad04b42cd8e89563b1e6c8fce6f86165fc26f
Arii <adomelier@alteca.fr>
2024-04-22 16:30:49 +0200

    clean useless filesé

--- WA_Seats - Copie.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="WA_Seats - Copie" tilewidth="32" tileheight="32" tilecount="210" columns="14">
- <image source="tilesets/WA_Seats - Copie.png" width="454" height="510"/>
-</tileset>
